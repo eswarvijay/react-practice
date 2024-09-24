486f7d0ad86bbd6b73f2849c7c2763f47c7a0300
eswarvijay <147239791+eswarvijay@users.noreply.github.com>
2024-09-24 10:11:16 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Helloworld from './Helloworld'
+function App() {
+  return (
+    <div>
+      <Helloworld/>
+    </div>
+  )
+}
+
+export default App
+
---
+++ Helloworld.jsx
@@ -0,0 +1,10 @@
+import react from "react"
+function Helloworld()
+{
+    return(
+        <div>
+            <h1>Hello world</h1>
+        </div>
+    )
+}
+export default Helloworld;
